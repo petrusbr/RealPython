217bf515532c47f7abfe30a3aaf74b86dcf89fca
Pedro Teixeira <pedrao@gmail.com>
2016-11-03 19:47:04 +0000

    Finish Tasks operations

---
+++ flasktaskr/project/forms.py
@@ -0,0 +1,18 @@
+# project/forms.py
+
+from flask_wtf import Form
+from wtforms import StringField, DateField, IntegerField, SelectField
+from wtforms.validators import DataRequired
+
+class AddTaskForm(Form):
+    task_id = IntegerField()
+    name = StringField('Nome da Tarefa', validators=[DataRequired()])
+    due_date = DateField('Data de Término (dd/mm/yyyy)',
+                          validators=[DataRequired()], format='%d/%m/%Y'
+                         )
+    priority = SelectField('Priority', validators=[DataRequired()],
+                            choices=[('1', '1'), ('2', '2'), ('3', '3'), 
+                            ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), 
+                            ('8', '8'), ('9', '9'), ('10', '10')]
+                           )
+    status = IntegerField('Status')
--- flasktaskr/project/views.py
+++ flasktaskr/project/views.py
@@ -2,9 +2,10 @@
 import sqlite3
 
 from flask import Flask, flash, redirect, render_template, \
-     request, session, url_for
+     request, session, url_for, g
 
 from functools import wraps
+from forms import AddTaskForm
 
 #Configuracao
 
@@ -33,7 +34,7 @@ def login_required(test):
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    flash('Voce foi deslogado')
+    flash('Voce foi deslogado. Au revoir!')
     return redirect(url_for('login'))
 
 @app.route('/', methods=['GET', 'POST'])
@@ -48,6 +49,72 @@ def login():
             session['logged_in'] = True
             flash("Bem-vindo!")
             return redirect(url_for('tasks'))
-    # If method = 'GET'
+    # If method = 'GET' - Se o metodo eh uma HTTP GET
     return render_template('login.html')
     
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=1')
+    open_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) 
+        for row in cur.fetchall()
+    ]
+    
+    cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=0')
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) 
+        for row in cur.fetchall()
+    ]
+    g.db.close()
+    return render_template('tasks.html',
+                           form = AddTaskForm(request.form),
+                           open_tasks=open_tasks,
+                           closed_tasks=closed_tasks)
+    
+# Add New Tasks
+@app.route('/add/', methods=['POST'])
+@login_required
+def new_task():
+    g.db = connect_db()
+    name = request.form['name']
+    date = request.form['date']
+    priority = request.form['priority']
+    if not name or not date or not priority:
+        flash("Todos os campos devem ser preenchidos. Por favor, tente de novo")
+        return redirect(url_for('tasks'))
+    else:
+        g.db.execute('insert into tasks (name, due_date, priority, status)' \
+             'values (?, ?, ?, 1)', [
+                 request.form['name'], 
+                 request.form['due_date'], 
+                 request.form['priority']
+                 ]
+        )
+        g.db.commit()
+        g.db.close()
+        flash("Nova tarefa criada com sucesso!")
+        return redirect(url_for('tasks'))
+        
+# Mark tasks as complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.db.execute('update tasks set status = 0 where task_id=' + str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash("A tarefa foi marcada como completa.")
+    return redirect(url_for('tasks'))
+
+# Delete Tasks
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    g.db.execute('delete from tasks where task_id=' + str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash("A tarefa foi removida.")
+    return redirect(url_for('tasks'))
