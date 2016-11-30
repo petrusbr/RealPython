e17a71edca4fced87b6b0e634e1456d331656d3d
Pedro Teixeira <pedrao@gmail.com>
2016-11-30 21:00:11 -0200

    Templates

--- django-bloggy/bloggy/blog/views.py
+++ django-bloggy/bloggy/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+
+from blog.models import Post
 
 # Create your views here.
+def index(request):
+	ultimos_posts = Post.objects.all().order_by('-created_at')
+	t = loader.get_template('blog/index.html')
+	c = Context({'ultimos_posts': ultimos_posts, })
+	return HttpResponse(t.render(c))
--- django-bloggy/bloggy/bloggy/urls.py
+++ django-bloggy/bloggy/bloggy/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from blog import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', views.index, name='index'), 
 ]
