173f0340fab9ce0e60532967e0def8c5bc30402c
Pedro Teixeira <pedrao@gmail.com>
2016-11-30 18:48:49 +0000

    Begin templates

--- django-bloggy/bloggy/blog/tests.py
+++ django-bloggy/bloggy/blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from blog.models import Post
 
 # Create your tests here.
+class PostTests(TestCase):
+    
+    def test_str(self):
+        meu_titulo = Post(title='Este é um título de um caso de teste básico')
+        self.assertEquals(str(meu_titulo), 
+                          'Este é um título de um caso de teste básico'
+        )
